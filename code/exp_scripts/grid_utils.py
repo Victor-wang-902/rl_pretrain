8b9ffb0f1a269d16a94c842bf15ecb5c5c4c21da
watchernyu <cw1681@nyu.edu>
2023-04-15 15:31:02 -0700

    work on grid

--- code/exp_scripts/grid_utils.py
+++ code/exp_scripts/grid_utils.py
@@ -11,8 +11,8 @@ MUJOCO_ALL = ['halfcheetah-random-v2', 'halfcheetah-medium-v2', 'halfcheetah-exp
               'hopper-medium-replay-v2', 'hopper-medium-expert-v2',
               'ant-random-v2', 'ant-medium-v2', 'ant-expert-v2',
               'ant-medium-replay-v2', 'ant-medium-expert-v2']
-MUJOCO_3_ENVS = ['halfcheetah', 'walker2d', 'hopper']
-MUJOCO_3_DATASETS = ['medium','','',]
+MUJOCO_3_ENVS = ['hopper', 'walker2d', 'halfcheetah',  ]
+MUJOCO_3_DATASETS = ['medium','medium-replay','medium-expert',]
 MUJOCO_9 = ['halfcheetah-medium-v2', 'halfcheetah-medium-replay-v2', 'halfcheetah-medium-expert-v2',
             'walker2d-medium-v2', 'walker2d-medium-replay-v2', 'walker2d-medium-expert-v2',
             'hopper-medium-v2', 'hopper-medium-replay-v2', 'hopper-medium-expert-v2',
