dedec5e0c9e99e290e5fd8abaff32374f7c582fc
watchernyu <cw1681@nyu.edu>
2023-04-18 17:17:04 -0700

    debug

--- rlcode/experiments/debug/debugnew.py
+++ rlcode/experiments/debug/debugnew.py
@@ -23,13 +23,14 @@ if __name__ == '__main__':
 
     # each 3-tuple is:
     # parameter name - abbreviation - values (this has to be a list)
-    exp_prefix = 'il'
+    exp_prefix = 'debug'
     # env, dataset and seed values will be added to the end of the folder name string
     # for each 3-tuple,
     settings = [
         'env', '', MUJOCO_3_ENVS,
         'dataset', '', MUJOCO_3_DATASETS,
         'seed', '', [42, 666, 1024],
+        'agent','',['cql',],
         'debug', 'd', [True,]
     ]
 
