b44d85806cce3a4a0378adf0168f4ffd9d878b18
watchernyu <cw1681@nyu.edu>
2023-04-20 13:12:21 -0700

    debug

--- cqlcode/SimpleSAC/model.py
+++ cqlcode/SimpleSAC/model.py
@@ -250,9 +250,18 @@ class FullyConnectedQFunctionPretrain(nn.Module):
         return self.hidden_to_value(h)
 
     def forward(self, observations, actions):
+        multiple_actions = False
+        batch_size = observations.shape[0]
+        if actions.ndim == 3 and observations.ndim == 2:
+            multiple_actions = True
+            observations = extend_and_repeat(observations, 1, actions.shape[1]).reshape(-1, observations.shape[-1])
+            actions = actions.reshape(-1, actions.shape[-1])
+
         h = self.get_feature(observations, actions)
-        output = self.last_fc_layer(h)
-        return output
+        q_values = self.last_fc_layer(h)
+        if multiple_actions:
+            q_values = q_values.reshape(batch_size, -1)
+        return q_values
 
 
 class Scalar(nn.Module):
