e4604ba7d48ee6e17ed66659168d6f4196cea809
watchernyu <cw1681@nyu.edu>
2023-07-13 21:41:19 -0700

    watcher final

--- cqlcode/exp/new100/mdp_iid_bigdata.py
+++ cqlcode/exp/new100/mdp_iid_bigdata.py
@@ -36,7 +36,7 @@ def main():
     ###########################################################
     exp_prefix = 'cqlr3n'
     settings = [
-        'env', '', MUJOCO_3_ENVS,
+        'env', '', ['halfcheetah'],
         'dataset', '', MUJOCO_3_DATASETS,
         'pretrain_mode', 'pre', ['mdp_same_noproj'],  # 'none', 'q_sprime', 'mdp_q_sprime'
         'qf_hidden_layer', 'l', [2],
